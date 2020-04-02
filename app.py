c371d40259a79c58d5f96a7600fc4a4922302c88
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2020-04-02 15:16:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from os import environ
 from flask import abort, Flask, jsonify, make_response, request, send_from_directory, render_template
+import mysql.connector
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="Boss1234",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
 
 app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/predict', methods=['POST'])
-def create_client:    
+def create_client():    
+  sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+  val = ("John", "Highway 21")
+  mycursor.execute(sql, val)
+
+  mydb.commit()
+
+  print(mycursor.rowcount, "record inserted.")
