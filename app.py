322bfbf3e01e2fe76c209b7b3fc600437f3bb1e4
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2020-04-15 15:21:00 -0400

    app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ try:
     
     conn = connect (
         dbname = 'setmd',
-        user = "postgres",
+        user = "setmd",
         host = "setmd-db.ceij67ce3ano.us-east-2.rds.amazonaws.com",
-        password = "Boss1234"
+        password = "P455w0rd"
     )
 
     cur = conn.cursor()
@@ -34,34 +34,61 @@ app = Flask(__name__, template_folder='templates')
 def create_user():    
   dictionary = dict(json.loads(request.data))
   
+  
   name = dictionary["name"]
   email = dictionary["email"]
   phone = dictionary["phone"]
-  role = dictionary["type"]
+  role = dictionary["role"]
   disabled = dictionary["disabled"]
+  
+  
+  if role[-1] == 'y':
+      role = role.replace("y","ie")
+  role = role +'s'
     
-  sql = "INSERT INTO setmd.users (name, email, phone, type, disabled) VALUES (%s, %s, %s, %s, %s)"
-  val = (name,email,phone,role,disabled)
-  cur.execute(sql, val)
+  
+  query = "INSERT INTO setmd.users (name, email, phone, type, disabled) VALUES (%s, %s, %s, %s, %s)"
+  vals = (name,email,phone,role,disabled)
+  cur.execute(query, vals)
+  conn.commit()
 
   query = ("SELECT id FROM setmd.users WHERE name = %s")
+  cur.execute(query, name)
+  conn.commit()
+  result = cur.fetchall()
   
-  uid = cur.execute(query, name)
+  uid = result[0][0]
+  role = result[0][1]
   print(uid)
+  
+  
+  
   
   
 def create_user_test():    
   cur = conn.cursor()
-  sql2 = "INSERT INTO setmd.users (name, email, phone, role, disabled) VALUES ('Jon Tonthat', 'jwtonthat@gmail.com', 9783176486, 'GOD', false)"
-  cur.execute(sql2)
-
-  query = ("SELECT id FROM setmd.users WHERE name = 'jon'")
   
-  uid = cur.execute(query)
+  '''
+  query = "INSERT INTO setmd.users (name, email, phone, role, disabled) VALUES ('Jon Tonthat', 'jwtonthat@gmail.com', 9783176486, 'GOD', false)"
+  cur.execute(query)
+  conn.commit()
+  '''
+  
+  query = ("SELECT id, role FROM setmd.users WHERE name = 'Jon Tonthat'")
+  cur.execute(query)
+  result = cur.fetchall()
+  
+  
+  uid = result[0][0]
+  role = result[0][1]
   print(uid)
+
+  types_of_roles = ["doctors", "patients", "productioncompanies", "productioncoordinators", "users", "setmedics"]
+  if role in types_of_roles:
+      print("yay")
 
   
 if __name__ == '__main__':
-    create_user_test()
+    #create_user_test()
     app.run(host='0.0.0.0', port=5001, debug=False, threaded=False)
     
